27540b3edf70792e0060803361f74e129054646b
Simone <64407526+Bauda@users.noreply.github.com>
2021-04-27 02:44:57 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 #checksum
 
-fc = input("Enter your fiscal code. Be careful, it must consist of exactly 15 characters.\n")
+import dictionary
+
+fc = input("Enter your fiscal code. Be careful, it must consist of exactly 15 characters. Any special character (e.g. '@') will be automatically deleted.\n")
+
+chars = "\`*_{@}[]()>#+-.!$ "
+
+for x in chars:
+    fc = fc.replace(x, "")
 
 while (len(fc) != 15):
-    fc = input("The fiscal code must be composed of 15 characters. Yours only contains " + str(len(fc)) + ".\n")
-    if (len(fc) != 15):
-        continue
-    else:
-        break
+    fc = input("The fiscal code must consist of 15 characters. Yours contains " + str(len(fc)) + ".\n")
+    continue
 
 n = 0
 even = []
@@ -18,15 +22,10 @@ for n, x in enumerate(fc, start = 1):
     else:
         even.append(x)
 
-import dictionary
-
 even_characters = [(dictionary.even).get(char_eve, char_eve) for char_eve in even]
 odd_characters = [(dictionary.odd).get(char_odd, char_odd) for char_odd in odd]
 
-even_characters = [x for x in map(int, even_characters)]
-odd_characters = [x for x in map(int, odd_characters)]
-
-module = str((sum(even_characters) + sum(odd_characters)) % 26)
+module = (sum(even_characters) + sum(odd_characters)) % 26
 
 checksum = dictionary.remainder.get(module)
 print("Done! The checksum is \"" + checksum + "\".\nFull fiscal code: " + fc + checksum + ".")
